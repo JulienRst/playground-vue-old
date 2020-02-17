22dbc7f1d2b9605856695d52876e3dbea8eba057
Julien Rousset <j.rousset@groupeonepoint.com>
2020-02-17 21:46:55 +0100

    Initialize VueJs with TS and my config

---
+++ src/views/Root.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>RootView is working !</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+	name: 'RootView'
+})
+export default class RootView extends Vue {
+	public mounted () {
+		console.log('RootView has been properly mounted');
+	}
+}
+</script>
