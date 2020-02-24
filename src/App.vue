3692a58e943ceaabe3ca39314f7aaf9e747c0b85
Julien Rousset <j.rousset@groupeonepoint.com>
2020-02-25 00:02:56 +0100

    [Console] Add fancy console logging

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,10 @@ import { Component, Vue } from 'vue-property-decorator';
 })
 export default class App extends Vue {
 	public mounted () {
-		console.log('App View has been properly mounted');
+		console.log('%cJulien Rousset | PlayGround', 'padding: 3px; background-color: white; color: #101010');
+		console.log('%cMail          %cjulien.rousset01@gmail.com', 'padding: 3px; background-color: white; color: #101010', ' padding: 3px; background-color: white; color: black');
+		console.log('%cPhone         %c+33608957135', 'padding: 3px; background-color: white; color: #101010', ' padding: 3px; background-color: white; color: black');
+		console.log('%cTwitter       %c@JulienRst', 'padding: 3px; background-color: white; color: #101010', ' padding: 3px; background-color: white; color: black');
 	}
 }
 </script>
